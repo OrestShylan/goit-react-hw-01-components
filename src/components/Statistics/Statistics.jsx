09b93abf86a1533634a0e34684366198cd72e0c1
OrestShylan <112808022+OrestShylan@users.noreply.github.com>
2023-03-17 17:45:07 +0200

    update

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Statistics = ({ title, stats }) => {
+  console.log({ stats });
+  return (
+    <section>
+      <h2 class="title">Upload stats</h2>
+      <ul></ul>
+    </section>
+  );
+};
+export default Statistics;
