d8e6f3b7911337341c958ad2de661cbcbf9ed353
OrestShylan <112808022+OrestShylan@users.noreply.github.com>
2023-03-18 10:20:08 +0200

    addet transaction and static

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,21 @@ import Profile from './Profile/Profile';
 import user from './Profile/user.json';
 import Statistics from './Statistics/Statistics';
 import data from './Statistics/data.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from './TransactionHistory/transactions.json';
 
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics stats={data} />
+      <TransactionHistory items={transactions} />
+    </div>
   );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,9 +5,30 @@ const Statistics = ({ title, stats }) => {
   console.log({ stats });
   return (
     <section>
+      {title && <h2>{title}</h2>}
       <h2 class="title">Upload stats</h2>
-      <ul></ul>
+      <ul>
+        {stats.map(stat => {
+          return (
+            <li key={stat.id}>
+              <span>{stat.label}</span>
+              <span>{stat.percentage}</span>
+            </li>
+          );
+        })}
+      </ul>
     </section>
   );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
 };
 export default Statistics;
